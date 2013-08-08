c8ca1b6177ff4ede4e3630b429226cca2d45bb7d
Ken Rickard <agentrickard@gmail.com>
2013-08-08 19:05:26 -0400

    Testing stub for review.

---
+++ tests/Drupal/drupalgotchi/Tests/Plugin/Action/SetDrupalgotchiTest.php
@@ -0,0 +1,71 @@
+<?php
+
+/**
+* @file
+* Contains \Drupal\drupalgotchi\Tests\Plugin\Action\SetDrupalgotchiTest.
+*/
+
+namespace Drupal\drupalgotchi\Tests\Plugin\Action;
+
+use Drupal\Tests\UnitTestCase;
+use Drupal\drupalgotchi\Plugin\Action\SetDrupalgotchi;
+
+  /**
+   * Tests the role add plugin.
+   *
+   * @see \Drupal\user\Plugin\Action\AddRoleUser
+   */
+class SetDrupalgotchiTest extends UnitTestCase {
+
+  /**
+   * The key/value store.
+   */
+  protected $state;
+
+  public static function getInfo() {
+    return array(
+      'name' => 'Drupalgotchi action plugin',
+      'description' => 'Tests the set Drupalgotchi plugin',
+      'group' => 'Drupalgotchi',
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+
+    // Autoloading is not working for contrib. Load our class to test.
+    // See https://drupal.org/node/2025883
+    include_once DRUPAL_ROOT . '/modules/drupalgotchi/lib/Drupal/drupalgotchi/Plugin/Action/SetDrupalgotchi.php';
+
+    // This part does not seem to be working.
+    $this->stub = $this
+      ->getMockBuilder('Drupal\Core\KeyValueStore\KeyValueStoreInterface')
+      ->getMock();
+    // Configure the stub.
+    $this->stub->expects($this->any())
+      ->method('set')
+      ->will($this->returnValue(10));
+    $this->stub->expects($this->any())
+      ->method('get')
+      ->will($this->returnValue(10));
+
+  }
+
+  /**
+   * Tests the execute method for setting state value.
+   */
+  public function testSet() {
+    $config = array('name' => 'foo', 'needy' => 10);
+    $set = new SetDrupalgotchi($config, 'drupalgotchi_set_attention', array(), $this->stub);
+    // Fails, doing nothing because the method doesn't return anything.
+    $this->assertEquals(10, $set->execute(10));
+    // Passes, but useless, because it bypasses our class.
+    $this->assertEquals(10, $this->stub->get('drupalgotchi.attention'));
+
+  }
+
+}
+

b7c51912fae8e02e71d6b0f19a792e0c41583ec8
Larry Garfield <larry@garfieldtech.com>
2013-08-14 01:00:04 -0500

    Strip down to the event skeleton.

--- lib/Drupal/drupalgotchi/EventSubscriber/DrupalgotchiSubscriber.php
+++ lib/Drupal/drupalgotchi/EventSubscriber/DrupalgotchiSubscriber.php
@@ -17,40 +17,8 @@ use Drupal\Core\StringTranslation\TranslationManager;
  */
 class DrupalgotchiSubscriber implements EventSubscriberInterface {
 
-  /**
-   * The state system.
-   *
-   * @var \Drupal\Core\KeyValueStore\KeyValueStoreInterface
-   */
-  protected $state;
+  public function __construct() {
 
-  /**
-   * The configuration object.
-   *
-   * @var \Drupal\Core\Config\Config
-   */
-  protected $config;
-
-  /**
-   * The translation system.
-   *
-   * @var \Drupal\Core\StringTranslation\TranslationManager
-   */
-  protected $translator;
-
-  /**
-   *
-   * @param \Drupal\Core\Config\ConfigFactory $config_factory
-   *   The configuration system.
-   * @param \Drupal\Core\KeyValueStore\KeyValueStoreInterface $state
-   *   The state storage system.
-   * @param \Drupal\Core\StringTranslation\TranslationManager $translator
-   *   The translation system.
-   */
-  public function __construct(ConfigFactory $config_factory, KeyValueStoreInterface $state, TranslationManager $translator) {
-    $this->config = $config_factory->get('drupalgotchi.settings');
-    $this->state = $state;
-    $this->translator = $translator;
   }
 
   /**
@@ -60,29 +28,7 @@ class DrupalgotchiSubscriber implements EventSubscriberInterface {
    *   The system event.
    */
   public function onKernelRequestSetHappiness(GetResponseEvent $event) {
-    if ($event->getRequestType() != KernelInterface::MASTER_REQUEST) {
-      return;
-    }
 
-    // Note: there's an issue here because Drupal makes THREE requests per page.
-    // One for the page, one for toolbar, one for contextual links. This is a
-    // decent way around that.
-    if ($event->getRequest()->attributes->get('_controller') != 'controller.page:content') {
-      return;
-    }
-
-    $attention_quotient = $this->state->get('drupalgotchi.attention') ?: 0;
-
-    if ($event->getRequest()->attributes->get('_account')->hasPermission('make drupalgotchi happy')) {
-      $neediness = $this->config->get('needy');
-      $change = 10 - $neediness;
-      $attention_quotient += $change;
-    }
-    else {
-      $attention_quotient -= 1;
-    }
-
-    $this->state->set('drupalgotchi.attention', $attention_quotient);
   }
 
   /**
@@ -92,24 +38,7 @@ class DrupalgotchiSubscriber implements EventSubscriberInterface {
    *   The system event.
    */
   public function onKernelRequestShowHappiness(GetResponseEvent $event) {
-    if ($event->getRequestType() != KernelInterface::MASTER_REQUEST) {
-      return;
-    }
 
-    // Note: there's an issue here because Drupal makes THREE requests per page.
-    // One for the page, one for toolbar, one for contextual links. This is a
-    // decent way around that.
-    if ($event->getRequest()->attributes->get('_controller') != 'controller.page:content') {
-      return;
-    }
-
-    $attention_quotient = $this->state->get('drupalgotchi.attention') ?: 0;
-    if ($attention_quotient <= 0) {
-      $message = $this->translator->translate('@name misses its owner. Please come back! @name has a sad. :-(', array(
-        '@name' => $this->config->get('name'),
-      ));
-      drupal_set_message($message);
-    }
   }
 
   /**

56419c525bf27dd785b8d3c0ba6da1ff89a5e555
Larry Garfield <garfield@palantir.net>
2013-09-19 18:15:43 -0500

    Correct multi-request bug the right way this time.

--- lib/Drupal/drupalgotchi/EventSubscriber/DrupalgotchiSubscriber.php
+++ lib/Drupal/drupalgotchi/EventSubscriber/DrupalgotchiSubscriber.php
@@ -4,11 +4,11 @@ namespace Drupal\drupalgotchi\EventSubscriber;
 
 use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
-use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\KernelInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Cmf\Component\Routing\RouteObjectInterface;
 
 use Drupal\Core\KeyValueStore\KeyValueStoreInterface;
-use Drupal\Core\Config\Config;
 use Drupal\Core\Config\ConfigFactory;
 use Drupal\Core\StringTranslation\TranslationManager;
 
@@ -64,11 +64,13 @@ class DrupalgotchiSubscriber implements EventSubscriberInterface {
       return;
     }
 
-    // Note: there's an issue here because Drupal makes THREE requests per page.
+    // Note: there's an issue here because Drupal makes multiple requests per page.
     // One for the page, one for toolbar, one for contextual links. This is a
-    // decent way around that.
-    if ($event->getRequest()->attributes->get('_controller') != 'controller.page:content') {
-      //return;
+    // decent way around that for now.
+    $request = $event->getRequest();
+    $route_name = $request->attributes->get(RouteObjectInterface::ROUTE_NAME);
+    if (in_array($route_name, array('contextual.render'))) {
+      return;
     }
 
     $attention_quotient = $this->state->get('drupalgotchi.attention') ?: 0;

0b518244ae08ed3266b466984db49450293044f1
Larry Garfield <garfield@palantir.net>
2013-09-20 15:43:57 -0500

    Add HelloWorld controller class.

---
+++ lib/Drupal/drupalgotchi/Controller/HelloController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Drupal\drupalgotchi\Controller;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Controller class for hello-world actions.
+ */
+class HelloController extends ControllerBase {
+
+  /**
+   * Route callable method.
+   *
+   * @return
+   *   A string that is the body of a page.
+   */
+  public function hello() {
+    return "Hello World";
+  }
+
+  /**
+   * Route callable method.
+   *
+   * @return \Symfony\Component\HttpFoundation\JsonResponse
+   *   A JSON response.
+   */
+  public function helloPage() {
+    $json['hello'] = 'World';
+
+    return new JsonResponse($json);
+  }
+
+  /**
+   * Route callable method.
+   *
+   * @param $person
+   *   The name to display.
+   *
+   * @return
+   *   A theme array. See hellodrupal-hello.html.twig.
+   */
+  public function helloPerson($person) {
+    return array(
+      '#theme' => 'drupalgotchi_hello',
+      '#person' => $person,
+    );
+  }
+
+}
